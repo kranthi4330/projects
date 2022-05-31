43a1dddaca4ed0b8d0edf622ac59ed3d496ebd73
kranthi4330 <90904317+kranthi4330@users.noreply.github.com>
2022-05-31 13:53:37 +0530

    Add files via upload

---
+++ Changecase.java
@@ -0,0 +1,23 @@
+package Kra2;
+
+public class Changecase {
+
+	public static void main(String[] args) {
+			String s1="java1@";
+			String s2=" ";
+			for( int i=0;i<s1.length();i++)
+			{
+				char c=s1.charAt(i);
+				if(c>='A'&&c<='Z')
+					s2+=(char)(c+32);
+				else if(c>='a'&&c<='z')
+					s2+=(char)(c-32);
+				else
+					s2+=c;
+					
+			}
+			System.out.println(s2);
+
+	}
+
+}
