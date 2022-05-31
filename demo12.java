b0038b006cf598e1b0963abe8826fbcfaa1e62f1
kranthi4330 <90904317+kranthi4330@users.noreply.github.com>
2022-05-31 13:50:11 +0530

    Add files via upload

---
+++ demo12.java
@@ -0,0 +1,15 @@
+package Package1;
+
+public class demo12 {
+
+	public static void main(String[] args) {
+		String s1="developer";
+		System.out.println(s1.substring(2));
+		System.out.println(s1.substring(2,6));
+		System.out.println(s1.contains("lop"));
+		System.out.println(s1.startsWith("de"));
+		System.out.println(s1.endsWith("per"));
+
+	}
+
+}
